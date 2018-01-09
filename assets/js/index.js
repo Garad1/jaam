e44dadb94673129c694d322524714b91bef6c7bf
Dranile <maxime.pyz@gmail.com>
2018-01-09 19:30:15 +0100

    close toast action

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,10 @@
 var $ = require('jquery');
 require('materialize-css');
+
+$(function () {
+    $('.js-close-toast').each(function (index, object) {
+        $(object).on('click', function(){
+            $(this.parentElement).fadeOut(300, function(){ $(this).remove();});
+        });
+    });
+});
